12b7aed7c63ed96e2b3c9fd7a28ac0a3fdeea352
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-09-18 17:40:45 +0200

    Update #17

--- Untitled70/Untitled2.js
+++ Untitled70/Untitled2.js
@@ -1,3 +1,3 @@
 ﻿saasdasd
-asdasdasdassaddsadasdasdasvsdsjhfgaskjfgjaksgfkjfsjkdgfjsdgfdzxczxxzcasdas   sdfdadsadsdasdsaddass
+asdasdasdasjsfasfjasdfjsaddsadasdasdasvsdsjhfgaskjfgjaksgfkjfsjkdgfjsdgfdzxczxxzcasdas   sdfdadsadsdasdsaddass
 jasjfkgsdjf
